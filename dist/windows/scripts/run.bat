dae1537ada2d8db4720a6180edf698a6eca330a4
Ali <ali.nasrollahpour2006@gmail.com>
2025-07-07 18:05:55 +0330

    edit to packager and packages

--- dist/windows/scripts/run.bat
+++ dist/windows/scripts/run.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d %~dp0..
-java --module-path "jfx\windows\lib" --add-modules javafx.controls,javafx.fxml -jar InsTax!.jar
+java --module-path "lib" --add-modules javafx.controls,javafx.fxml -jar InsTax!.jar
 pause
